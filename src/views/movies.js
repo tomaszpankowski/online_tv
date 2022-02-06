e96e94839b7c8962bfc70dcb38966ede3ba0ca68
tomaszpankowski <pankowski.email@gmail.com>
2022-02-06 11:12:07 +0100

    layout done

--- src/views/movies.js
+++ src/views/movies.js
@@ -1,11 +1,18 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import MoviesS1 from "../components/movies-s1";
+import MoviesS2 from "../components/movies-s2";
+import MoviesS3 from "../components/movies-s3";
+import MoviesS4 from "../components/movies-s4";
 
 class Movies extends Component{
     render(){
         return(        
-            <Container fluid className="movies-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <MoviesS1/>
+                <MoviesS2/>
+                <MoviesS3/>
+                <MoviesS4/>
             </Container>    
         );
     }
